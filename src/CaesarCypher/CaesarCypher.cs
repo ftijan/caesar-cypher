003768320b9b8c6b76d8559e05fa0c45cd64bd5c
Filip Tijan <filip.tijan@greenfinch.ie>
2020-06-13 21:17:12 +0100

    more skeleton code

--- src/CaesarCypher/CaesarCypher.cs
+++ src/CaesarCypher/CaesarCypher.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace CaesarCypher
 {
@@ -13,6 +15,40 @@ namespace CaesarCypher
                 throw new ArgumentNullException(nameof(cypher));
             }
 
+            var cypherMap = GetOffsetAlphabetMap(BaseAlphabet, offset);
+
+            var decryptedPhrase = new StringBuilder();
+
+            foreach (var character in cypher)
+            {
+                if (character == ' ')
+                {
+                    decryptedPhrase.Append(' ');
+                }
+                else
+                {
+                    // Assume correct alphabet with correct letter for this exercise
+                    decryptedPhrase.Append(Char.ToUpperInvariant(cypherMap[Char.ToLowerInvariant(character)]));
+                }
+            }
+
+            return decryptedPhrase.ToString();
+        }
+
+        private static IDictionary<char,char> GetOffsetAlphabetMap(char[] alphabet, int offset)
+        {
+            var offsetDictionaryMap = new Dictionary<char, char>();
+
+            for (int i = 0; i < alphabet.Length; i++)
+            {
+                offsetDictionaryMap.Add(alphabet[i], GetShiftedCharacter(alphabet, offset, i));
+            }
+
+            return offsetDictionaryMap;
+        }
+
+        private static char GetShiftedCharacter(char[] alphabet, int offset, int currentPosition)
+        {
             throw new NotImplementedException();
         }
     }
