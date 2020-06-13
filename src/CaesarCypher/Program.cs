9313853055a55697d3065f2c619dc128b58dca7a
Filip Tijan <filip.tijan@greenfinch.ie>
2020-06-14 00:00:40 +0100

    updated main method sample logic

--- src/CaesarCypher/Program.cs
+++ src/CaesarCypher/Program.cs
@@ -2,17 +2,30 @@
 
 namespace CaesarCypher
 {
+    /// <summary>
+    /// Demonstrates how to use the Caesar cypher logic.
+    /// </summary>
     class Program
     {
+        /// <summary>The app entry point.</summary>
+        /// <param name="args">Input params.</param>
         static void Main(string[] args)
         {
-            const int offset = 3;
-            var cypher = "ZKHHO ELFBFOH PDALPXP SURWHFW";
-
             var decryptor = new CaesarCypher();
+
+            // Working example:
+            var offset = 23;
+            var cypher = "QEB NRFZH YOLTK CLU GRJMP LSBO QEB IXWV ALD";
+            var plaintext = "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG";
+
             var decryptedPhrase = decryptor.DecryptWithOffset(cypher, offset);
 
-            Console.WriteLine($"With offset {offset}\nThe phrase\n{cypher}\nGives decrypted phrase:\n{decryptedPhrase}\n");
+            if (decryptedPhrase != plaintext)
+            {
+                Console.WriteLine("Incorrect logic implemented");
+            }            
+
+            Console.WriteLine($"With offset: {offset}\nThe phrase:\n{cypher}\nGives decrypted phrase:\n{decryptedPhrase}\n");
         }
     }
 }
