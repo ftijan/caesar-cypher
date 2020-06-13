80e2d57f7c21f7dcdf703d7e967377a2b8cb09f0
Filip Tijan <filip.tijan@greenfinch.ie>
2020-06-13 21:01:39 +0100

    added skeleton cypher class

---
+++ src/CaesarCypher/CaesarCypher.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CaesarCypher
+{
+    public class CaesarCypher
+    {
+        private static char[] BaseAlphabet = new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+
+        public string DecryptWithOffset(string cypher, int offset)
+        {
+            if (cypher is null)
+            {
+                throw new ArgumentNullException(nameof(cypher));
+            }
+
+            throw new NotImplementedException();
+        }
+    }
+}
