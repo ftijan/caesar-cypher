c390d41ba71666267febae464f4d77c62f649a2e
Filip Tijan <filip.tijan@greenfinch.ie>
2020-06-13 21:07:21 +0100

    test code in main method

--- src/CaesarCypher/Program.cs
+++ src/CaesarCypher/Program.cs
@@ -6,7 +6,13 @@ namespace CaesarCypher
     {
         static void Main(string[] args)
         {
-            
+            const int offset = 3;
+            var cypher = "ZKHHO ELFBFOH PDALPXP SURWHFW";
+
+            var decryptor = new CaesarCypher();
+            var decryptedPhrase = decryptor.DecryptWithOffset(cypher, offset);
+
+            Console.WriteLine($"With offset {offset}\nThe phrase\n{cypher}\nGives decrypted phrase:\n{decryptedPhrase}\n");
         }
     }
 }
